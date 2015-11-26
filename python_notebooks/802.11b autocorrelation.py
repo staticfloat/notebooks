cf434841ff45b8536f0b75434f30a93a3fb369e0
Elliot Saba <staticfloat@gmail.com>
2015-11-25 17:09:24 -0800

    Do I even use this anymore?

---
+++ python_notebooks/802.11b autocorrelation.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <markdowncell>
+
+# Why straight-up WiBreathe doesn't work with DSSS packets
+# ========================================================
+# 
+# In WiBreathe, we had a very special situation: we transmitted a sinusoidal signal in a multipath environment, and measured the rate of change of the multipath components indirectly by estimating the small-scale fading of the environment.  This is made possible mostly through
+# 
+
+# <codecell>
+
+
